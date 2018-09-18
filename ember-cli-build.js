604e32a282a40ae43bf2e410f49949a1f1f7df4c
Ryan Mark <rgmark@gmail.com>
2018-09-18 12:15:41 -0700

    ember-cli-update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,8 @@
+'use strict';
 /* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon({
@@ -11,7 +14,7 @@ module.exports = function(defaults) {
   });
 
   /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* jshint node: true */
 'use strict';
 
 var path = require('path');
@@ -6,7 +5,7 @@ var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
-  name: 'ember-bootstrap-datetimepicker',
+  name: require('./package').name
 
   included: function(target) {
     this._super.included.apply(this, arguments);
